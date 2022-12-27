b1b6745d38d664894a34459e88ff32a8dc7db3f3
spencer-rafada <spencer.byui@gmail.com>
2022-12-27 15:12:38 -0700

    committed day 12 part 1

--- DigitalGarden.playground/Pages/Day 11.xcplaygroundpage/Contents.swift
+++ DigitalGarden.playground/Pages/Day 11.xcplaygroundpage/Contents.swift
@@ -30,3 +30,27 @@ if success {
 }
 
 // TODO: Day 11 part 2
+struct Car {
+    private let model: String
+    private let numberOfSeats: Int
+    var currentGear = 0
+    
+    init(model: String, numberOfSeats: Int) {
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+    }
+    
+    mutating func changeGear(change: String) {
+        if change == "up" {
+            self.currentGear += 1
+        }
+        else if change == "down" {
+            self.currentGear -= 1
+        }
+    }
+}
+
+var dodge = Car(model: "Caliber", numberOfSeats: 4)
+print(dodge)
+dodge.changeGear(change: "up")
+print(dodge)
---
+++ DigitalGarden.playground/Pages/Day 12.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+//: [Previous](@previous)
+
+import Foundation
+
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
+
+// Inheritance
+class Employee {
+    let hours: Int
+
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+
+final class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+    // Override: keyword used to change an inherited method
+    override func printSummary() {
+        print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
+    }
+}
+
+// Final: keyword used to define that the class cannot be inherited from.
+final class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+let novall = Developer(hours: 8)
+novall.printSummary()
+
+// TODO: Continue on 'How to add initializers for classes
+//: [Next](@next)
